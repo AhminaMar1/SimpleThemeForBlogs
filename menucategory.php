28c1cbd1a58bab559702fe5736969d681f4c494b
Merro <MerroTheMerro@gmail.com>
2019-11-19 17:00:14 +0100

    Simple theme. Complete. First commit

---
+++ menucategory.php
@@ -0,0 +1,75 @@
+<a href="category.php?c=Diaries">
+مذكرات 
+</a>
+<a href="category.php?c=Diaries">
+يومي 
+</a>
+<a href="category.php?c=Diaries">
+يومياتي 
+</a>
+<a href="category.php?c=education">
+التعليم 
+</a>
+<a href="category.php?c=tips">
+خدعة 
+</a>
+<a href="category.php?c=tips">
+نصيحة 
+</a>
+<a href="category.php?c=tips">
+نصائح 
+</a>
+<a href="category.php?c=Ideas">
+فكرة 
+</a>
+<a href="category.php?c=design">
+التصميم 
+</a>
+<a href="category.php?c=Diaries">
+يوميات 
+</a>
+<a href="category.php?c=freelancing">
+العمل الحر 
+</a>
+<a href="category.php?c=education">
+تعليم 
+</a>
+<a href="category.php?c=entrepreneurship">
+ريادة أعمال 
+</a>
+<a href="category.php?c=Ideas">
+أفكار 
+</a>
+<a href="category.php?c=others">
+متفرقات 
+</a>
+<a href="category.php?c=marketing">
+التسويق 
+</a>
+<a href="category.php?c=updates">
+تحديثات 
+</a>
+<a href="category.php?c=exp">
+تجارب 
+</a>
+<a href="category.php?c=marketing">
+التسويق الالكتروني 
+</a>
+<a href="category.php?c=books">
+كتب 
+</a>
+<a href="category.php?c=tech">
+تقنية 
+</a>
+<a href="category.php?c=entrepreneurship">
+ريادة الأعمال 
+</a>
+<a href="category.php?c=content">
+تدوين 
+</a>
+<a href="category.php?c=design">
+تصميم 
+</a>
+<a href="category.php?c=Programming">
+برمجة 
+</a>
---
+++ profile.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>مدوِن العرب</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+</head>
+<body>
+<div class="minW">
+<header>
+	<div class="headerCont">
+		<a href="index.php"><div class="rightHead"><h1>مدون العرب</h1></div></a>
+		<div class="leftHead"><i class="fas fa-bars myfabar"></i></div>
+	</div>
+	<div id="menuHeader" class="menuHeader">
+		<div class="maxMenuHeader">
+			<?php include"menucategory.php"; ?>
+		</div>
+	</div>
+</header>
+<main>
+	<div class="scr">
+		<div class="prespace" id="prespace"></div>
+
+		<div class="profileUser">
+			<img src="https://i.suar.me/JL30o">
+			<b>طارق ناصر</b>
+			<p>طارق ناصر، مدوّن مهتم بمجالات عديدة طارق ناصر، مدوّن مهتم بمجالات عديدة طارق ناصر، مدوّن مهتم بمجالات عديدة</p>
+			<div class="link"><a href="https://tareknac.wordpress.com/" target="_blank">https://tareknac.wordpress.com/</a></div>
+		</div>
+
+
+
+		<div class="index">
+
+			<div class="indexCont">
+				<div class="pathAndDate">
+					<div class="path"><span>>></span>ريادة الأعمال</div>
+					<div class="date">31-10-2019</div>
+				</div>
+				<div class="titleCont">صححوا إحصائيات القراءة في الوطن العربي يرحمكم الله!</div>
+				<div class="contArti">صفحة عادية، فقط، لم تتلطخ بالحبر، لا تحمل تصنيفاً، ولا يمكن أن تؤذي أحداً، ربما البعض يشعر بالارتياح تجاه هذه الورقة، فيما البعض تضيق الكلمات في جوفه بشكل خانق. يجب أن تتسخ هذه الورقة ومثيلاتها بالكلمات وتصبح من مجرد كيان مجرد خالٍ المعاني إلى أداة معرفة، ومن الحياد الذي لا ....</div>
+				<div class="moreInf">
+					
+					<div class="user">
+						<a href="profile.php"><img src="https://i.suar.me/JL30o"></a>
+						<a href="profile.php"><p>طارق ناصر<p></p></a>
+					</div>
+
+					<div class="moreAndSee">		
+						<div class="see"><span>12</span><i class="fas fa-eye faeye"></i></div>
+						<a href="" alt=""><div class="more">إنتقل للقراءة</div></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="indexCont">
+				<div class="pathAndDate">
+					<div class="path"><span>>></span>ريادة الأعمال</div>
+					<div class="date">31-10-2019</div>
+				</div>
+				<div class="titleCont">صححوا إحصائيات القراءة في الوطن العربي يرحمكم الله!</div>
+				<div class="contArti">صفحة عادية، فقط، لم تتلطخ بالحبر، لا تحمل تصنيفاً، ولا يمكن أن تؤذي أحداً، ربما البعض يشعر بالارتياح تجاه هذه الورقة، فيما البعض تضيق الكلمات في جوفه بشكل خانق. يجب أن تتسخ هذه الورقة ومثيلاتها بالكلمات وتصبح من مجرد كيان مجرد خالٍ المعاني إلى أداة معرفة، ومن الحياد الذي لا ....</div>
+				<div class="moreInf">
+					
+					<div class="user">
+						<a href="profile.php"><img src="https://i.suar.me/JL30o"></a>
+						<a href="profile.php"><p>طارق ناصر<p></p></a>
+					</div>
+
+					<div class="moreAndSee">		
+						<div class="see"><span>12</span><i class="fas fa-eye faeye"></i></div>
+						<a href="" alt=""><div class="more">إنتقل للقراءة</div></a>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="indexCont">
+				<div class="pathAndDate">
+					<div class="path"><span>>></span>ريادة الأعمال</div>
+					<div class="date">31-10-2019</div>
+				</div>
+				<div class="titleCont">صححوا إحصائيات القراءة في الوطن العربي يرحمكم الله!</div>
+				<div class="contArti">صفحة عادية، فقط، لم تتلطخ بالحبر، لا تحمل تصنيفاً، ولا يمكن أن تؤذي أحداً، ربما البعض يشعر بالارتياح تجاه هذه الورقة، فيما البعض تضيق الكلمات في جوفه بشكل خانق. يجب أن تتسخ هذه الورقة ومثيلاتها بالكلمات وتصبح من مجرد كيان مجرد خالٍ المعاني إلى أداة معرفة، ومن الحياد الذي لا ....</div>
+				<div class="moreInf">
+					
+					<div class="user">
+						<a href="profile.php"><img src="https://i.suar.me/JL30o"></a>
+						<a href="profile.php"><p>طارق ناصر<p></p></a>
+					</div>
+
+					<div class="moreAndSee">		
+						<div class="see"><span>12</span><i class="fas fa-eye faeye"></i></div>
+						<a href="" alt=""><div class="more">إنتقل للقراءة</div></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="indexCont">
+				<div class="pathAndDate">
+					<div class="path"><span>>></span>ريادة الأعمال</div>
+					<div class="date">31-10-2019</div>
+				</div>
+				<div class="titleCont">صححوا إحصائيات القراءة في الوطن العربي يرحمكم الله!</div>
+				<div class="contArti">صفحة عادية، فقط، لم تتلطخ بالحبر، لا تحمل تصنيفاً، ولا يمكن أن تؤذي أحداً، ربما البعض يشعر بالارتياح تجاه هذه الورقة، فيما البعض تضيق الكلمات في جوفه بشكل خانق. يجب أن تتسخ هذه الورقة ومثيلاتها بالكلمات وتصبح من مجرد كيان مجرد خالٍ المعاني إلى أداة معرفة، ومن الحياد الذي لا ....</div>
+				<div class="moreInf">
+					
+					<div class="user">
+						<a href="profile.php"><img src="https://i.suar.me/JL30o"></a>
+						<a href="profile.php"><p>طارق ناصر<p></p></a>
+					</div>
+
+					<div class="moreAndSee">		
+						<div class="see"><span>12</span><i class="fas fa-eye faeye"></i></div>
+						<a href="" alt=""><div class="more">إنتقل للقراءة</div></a>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<div class="menu">
+			<div class="menuCont">
+				<div class="pathAndDate">
+					<div class="path">شيء ما</div>
+				</div>
+			</div>
+			<div class="menuCont">
+				<div class="pathAndDate">
+					<div class="path">التصنيفات</div>
+				</div>
+				<div class="menuSections">
+					<?php include"menucategory.php";?>
+				</div>
+			</div>
+			<div class="menuCont">
+				<div class="pathAndDate">
+					<div class="path">صفحتنا على الفيس بوك</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div style="clear: both; width: 10px; height: 1px;"></div>
+	<div class="btshp">
+		<a href="">
+			<div class="bt">
+				<i class="fas fa-angle-double-right faicon"></i>
+			</div>
+		</a>
+		<a href="">
+			<div class="bt">
+				<i class="fas fa-home faicon"></i>
+			</div>
+		</a>
+		<a href="">
+		<div class="bt">
+			<i class="fas fa-angle-double-left faicon"></i>
+		</div>
+		</a>
+	</div>
+
+	<div style="clear: both; width: 10px; height: 2px;"></div>
+</main>
+<footer>
+	<div class="lastFooter">
+		<h1>مدون العرب</h1>
+		<p>مدون العرب 2019-2020 | برمجة <a href=""> خالد </a> تصميم <a href="mailto:merrothemerro@gmail.com"> ميرو </a>
+	</div>
+</footer>
+</div>
+</body>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script type="text/javascript">
+
+
+$(function(){
+	$(".myfabar").click(function(){
+		$(".menuHeader").slideToggle();
+		$(".prespace").slideToggle();
+	})
+})
+
+function resizeHeightPrespace(event){
+	document.getElementById('menuHeader').style.display= 'block';
+	var offsetH= document.getElementById('menuHeader').offsetHeight;
+	document.getElementById('menuHeader').style.display= 'none';
+	var offsetHpx=offsetH+'px';
+	document.getElementById('prespace').style.height=offsetHpx;
+
+}
+window.onresize = function() {
+	resizeHeightPrespace();	
+};
+
+resizeHeightPrespace();
+</script>
+</html>
